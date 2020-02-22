b1a6ae8d67ee859c03a70b3452afb9220f718256
erik <erikj23.em@gmail.com>
2020-02-21 22:59:06 -0800

    + created test script for mnist data + added batching options

---
+++ load_images.m
@@ -0,0 +1,26 @@
+function images = load_images(filename)
+%loadMNISTImages returns a 28x28x[number of MNIST images] matrix containing
+%the raw MNIST images
+
+fp = fopen(filename, 'rb');
+assert(fp ~= -1, ['Could not open ', filename, '']);
+
+magic = fread(fp, 1, 'int32', 0, 'ieee-be');
+assert(magic == 2051, ['Bad magic number in ', filename, '']);
+
+image_count = fread(fp, 1, 'int32', 0, 'ieee-be');
+rows = fread(fp, 1, 'int32', 0, 'ieee-be');
+columns = fread(fp, 1, 'int32', 0, 'ieee-be');
+
+images = fread(fp, inf, 'unsigned char');
+images = reshape(images, columns, rows, image_count);
+images = permute(images,[2 1 3]);
+
+fclose(fp);
+
+% Reshape to #pixels x #examples
+images = reshape(images, size(images, 1) * size(images, 2), size(images, 3));
+% Convert to double and rescale to [0,1]
+images = double(images) / 255;
+
+end
