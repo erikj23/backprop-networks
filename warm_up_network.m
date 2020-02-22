db2053efeb93b19f26bafcd252ca7b61a40e0880
Mike Chavez <mc@Mikes-MacBook-Pro.local>
2020-02-21 17:44:54 -0800

    Fix initialization

--- warm_up_network.m
+++ warm_up_network.m
@@ -1,11 +1,7 @@
-r = neural_network;
-r.initialize(1, 2, @logsig);
-r.add_layer(3,2,@logsig);
-
 % build training input matrix
-p1=[-1	1	1	1	1	-1	1	-1	-1	-1	-1	1	1	-1	-1	-1	-1	1	1	-1	-1	-1	-1	1	-1	1	1	1	1	-1]';
-p2=[ -1	-1	-1	-1	-1	-1	1 -1 -1	-1	-1	-1	1	1	1	1	1	1	-1	-1	-1	-1	-1	-1	-1	-1	-1	-1	-1	-1]';
-p3=[1 -1 -1	-1	-1	-1	1 -1 -1	1	1	1	1	-1	-1	1	-1	1	-1	1	1	-1	-1	1	-1	-1	-1	-1	-1	1]';
+p1=[-1    1    1    1    1    -1    1    -1    -1    -1    -1    1    1    -1    -1    -1    -1    1    1    -1    -1    -1    -1    1    -1    1    1    1    1    -1]';
+p2=[ -1    -1    -1    -1    -1    -1    1 -1 -1    -1    -1    -1    1    1    1    1    1    1    -1    -1    -1    -1    -1    -1    -1    -1    -1    -1    -1    -1]';
+p3=[1 -1 -1    -1    -1    -1    1 -1 -1    1    1    1    1    -1    -1    1    -1    1    -1    1    1    -1    -1    1    -1    -1    -1    -1    -1    1]';
 P=[p1 p2 p3];
 
 % build expected output matrix
@@ -14,6 +10,14 @@ t2=[0 1 0]';
 t3=[0 0 1]';
 T=[t1 t2 t3];
 
-training_set = {P,T};
+training_set = {{p1 t1} {p2 t2} {p3 t3}};
 
-r.train(1, {{[1], 1+sin(pi/4)}});
+r = neural_network;
+
+% length = 30 neurons = 2 
+r.initialize(length(p1), 2, @logsig);
+
+% t = 3 so last layer has 3 neurons
+r.add_layer(3, @logsig);
+
+r.train(10, training_set);
