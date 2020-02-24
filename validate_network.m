00b058a49b41cf58d1b408a271d65a0f89dfef6d
erik <erikj23.em@gmail.com>
2020-02-23 19:11:24 -0800

    + added function to convert mnist labels to vectors + improved organization of validate_network.m (formerly: test_network.m) + minor changes in comments in neural_network.m and layer.m

---
+++ validate_network.m
@@ -0,0 +1,31 @@
+
+% reset enviornment
+clear
+
+% create training_set
+p = [1];
+t = 1+sin(pi/4);
+training_set = {{p t}};
+
+% set hyper-parameters
+epochs = 1;
+batch_size = 1;
+input_size = 1;
+input_neurons = 2;
+output_neurons = 1;
+
+% create network
+r = neural_network;
+r.initialize(input_size, input_neurons, @logsig);
+r.add_layer(output_neurons, @purelin);
+
+% pre-set intial weights & biases for layer 1
+r.layers{1}.w = [-0.27 -0.41]';
+r.layers{1}.b = [-0.48 -0.13]';
+
+% pre-set intial weights & biases for layer 1
+r.layers{2}.w = [0.09 -0.17];
+r.layers{2}.b = [0.48];
+
+% train network
+r.train(epochs, batch_size, training_set);
